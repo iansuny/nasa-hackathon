eb5a9594a37dd63258d8dd92d910dc48d836cdf9
孫合毅 <iansuny@gmail.com>
2017-04-27 20:40:54 +0800

    add tour api

---
+++ api/controllers/TourController.js
@@ -0,0 +1,36 @@
+/**
+ * TourController
+ *
+ * @description :: Server-side logic for managing tours
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+
+    createTour: function(req, res) {
+        var userId = req.session.userId;
+        var date = req.param('date');
+        var location = req.param('location');
+
+        Tour.create({
+                userId: userId,
+                date: date,
+                location: location,
+            })
+            .exec(function(err, tour) {
+                if (err) return res.negotiate(err);
+                return res.json({ result: 'ok' });
+            })
+    },
+
+    getToursByUserId: function(req, res) {
+        Tour.find({
+                userId: req.session.userId
+            })
+            .exec(function(err, tours) {
+                if (err) return res.negotiate(err);
+                return res.json({ result: tours });
+            })
+    },
+
+};
---
+++ api/models/Tour.js
@@ -0,0 +1,28 @@
+/**
+ * Tour.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/#!documentation/models
+ */
+
+module.exports = {
+
+    attributes: {
+
+        userId: {
+            type: 'string',
+            required: true
+        },
+
+        date: {
+            type: 'date',
+            required: true
+        },
+
+        location: {
+            type: 'string',
+            required: true
+        }
+
+    }
+};
