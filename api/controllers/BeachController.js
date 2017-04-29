dba3976f431c79d6a32edd763d688e3a99ef1ebf
norman kung <kungyc@tw.ibm.com>
2017-04-29 12:33:48 +0800

    fake inform

--- api/controllers/BeachController.js
+++ api/controllers/BeachController.js
@@ -25,7 +25,153 @@ module.exports = {
                     }
                     Promise.all(promiseArr).then(function() {
                         return res.json({
-                            result: result
+                            // result: result
+                            //fake
+                            result: [{
+                                "lat": 28.03754000222624,
+                                "lon": -82.87431374546918,
+                                "UVLevel": "low",
+                                "SeaSpeedLeveL": "low",
+                                "time": 1487635200000,
+                                "HABLevel": "hard",
+                                "name": "Clearwater Beach"
+                            }, {
+                                "lat": 28.03754000222624,
+                                "lon": -82.87431374546918,
+                                "UVLevel": "low",
+                                "SeaSpeedLeveL": "low",
+                                "time": 1487635200000,
+                                "HABLevel": "hard",
+                                "name": "Clearwater Beach"
+                            }, {
+                                "lat": 28.03754000222624,
+                                "lon": -82.87431374546918,
+                                "UVLevel": "low",
+                                "SeaSpeedLeveL": "low",
+                                "time": 1487635200000,
+                                "HABLevel": "hard",
+                                "name": "Clearwater Beach"
+                            }, {
+                                "lat": 28.03754000222624,
+                                "lon": -82.87431374546918,
+                                "UVLevel": "low",
+                                "SeaSpeedLeveL": "low",
+                                "time": 1487635200000,
+                                "HABLevel": "hard",
+                                "name": "Clearwater Beach"
+                            }, {
+                                "lat": 28.03754000222624,
+                                "lon": -82.87431374546918,
+                                "UVLevel": "low",
+                                "SeaSpeedLeveL": "low",
+                                "time": 1487635200000,
+                                "HABLevel": "hard",
+                                "name": "Clearwater Beach"
+                            }, {
+                                "lat": 28.03754000222624,
+                                "lon": -82.87431374546918,
+                                "UVLevel": "low",
+                                "SeaSpeedLeveL": "low",
+                                "time": 1487635200000,
+                                "HABLevel": "hard",
+                                "name": "Clearwater Beach"
+                            }, {
+                                "lat": 27.665151217072143,
+                                "lon": -82.69272168624656,
+                                "UVLevel": "low",
+                                "SeaSpeedLeveL": "low",
+                                "time": 1487635200000,
+                                "HABLevel": "hard",
+                                "name": "Fort Desoto"
+                            }, {
+                                "lat": 27.4853100452455,
+                                "lon": -82.68006526979174,
+                                "UVLevel": "low",
+                                "SeaSpeedLeveL": "low",
+                                "time": 1487635200000,
+                                "HABLevel": "hard",
+                                "name": "Coquina Beach"
+                            }, {
+                                "lat": 27.4853100452455,
+                                "lon": -82.68006526979174,
+                                "UVLevel": "low",
+                                "SeaSpeedLeveL": "low",
+                                "time": 1487635200000,
+                                "HABLevel": "hard",
+                                "name": "Coquina Beach"
+                            }, {
+                                "lat": 27.324282411580963,
+                                "lon": -82.59479604145403,
+                                "UVLevel": "low",
+                                "SeaSpeedLeveL": "low",
+                                "time": 1487635200000,
+                                "HABLevel": "hard",
+                                "name": "Lido Key"
+                            }, {
+                                "lat": 27.233515396172525,
+                                "lon": -82.53143992297512,
+                                "UVLevel": "low",
+                                "SeaSpeedLeveL": "low",
+                                "time": 1487635200000,
+                                "HABLevel": "hard",
+                                "name": "Siesta Key"
+                            }, {
+                                "lat": 27.780619755229388,
+                                "lon": -97.38122813230694,
+                                "UVLevel": "low",
+                                "SeaSpeedLeveL": "low",
+                                "time": 1487635200000,
+                                "HABLevel": "hard",
+                                "name": "Venice Beach"
+                            }, {
+                                "lat": 26.983975890410875,
+                                "lon": -82.40366514692131,
+                                "UVLevel": "low",
+                                "SeaSpeedLeveL": "low",
+                                "time": 1487635200000,
+                                "HABLevel": "hard",
+                                "name": "Manasota Beach"
+                            }, {
+                                "lat": 26.66834587946663,
+                                "lon": -82.26270368461019,
+                                "UVLevel": "low",
+                                "SeaSpeedLeveL": "low",
+                                "time": 1487635200000,
+                                "HABLevel": "hard",
+                                "name": "GI State Park(South Lighthouse)"
+                            }, {
+                                "lat": 26.46848856025335,
+                                "lon": -82.00840776813709,
+                                "UVLevel": "low",
+                                "SeaSpeedLeveL": "low",
+                                "time": 1487635200000,
+                                "HABLevel": "hard",
+                                "name": "Light House Beach Sanibel Island"
+                            }, {
+                                "lat": 26.266242029310966,
+                                "lon": -81.8583987194877,
+                                "UVLevel": "low",
+                                "SeaSpeedLeveL": "low",
+                                "time": 1487635200000,
+                                "HABLevel": "hard",
+                                "name": "Vanderbilt Beach"
+                            }, {
+                                "lat": 26.266242029310966,
+                                "lon": -81.8583987194877,
+                                "UVLevel": "low",
+                                "SeaSpeedLeveL": "low",
+                                "time": 1487635200000,
+                                "HABLevel": "hard",
+                                "name": "Vanderbilt Beach"
+                            }, {
+                                "lat": 25.96631497953799,
+                                "lon": -81.76400086337411,
+                                "UVLevel": "low",
+                                "SeaSpeedLeveL": "low",
+                                "time": 1487635200000,
+                                "HABLevel": "hard",
+                                "name": "South Marco Beach"
+                            }]
                         })
                     })
 
