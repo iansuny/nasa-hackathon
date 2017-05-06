fc5a73b5659439564e379bf3d5ccc933eb7e2e36
norman kung <kungyc@tw.ibm.com>
2017-05-06 23:51:33 +0800

    dataProcess

---
+++ api/services/informProcess/index.js
@@ -0,0 +1,9 @@
+var processHABData = require('./prcessHABData')
+var sliceInform = require('./sliceInform')
+var appendData = require('./append')
+
+module.exports = {
+    processHABData: processHABData,
+    sliceInform: sliceInform,
+    appendData: appendData
+}
